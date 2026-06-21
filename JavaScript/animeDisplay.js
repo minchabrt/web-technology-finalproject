1a1bcfc72f5fdce83bc083030671e57f51102102
Aleksa Komp <aleksamincic@gmail.com>
2026-06-21 12:46:25 +0200

    A lot of changes to the last commit; Added comments, "Login to Comment" button, back to top button;

--- JavaScript/animeDisplay.js
+++ JavaScript/animeDisplay.js
@@ -33,3 +33,40 @@ fetch(`https://api.jikan.moe/v4/anime?q=${searchQuery}`)
     });
  }
 
+let buttons = document.querySelectorAll(".like-button");
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        let likeImage = this.querySelector(".like-button-image");
+        
+        if (likeImage.src.includes('Images/like-button2.png')) {
+            likeImage.src = 'Images/like-button1.png';
+        } else {
+            likeImage.src = 'Images/like-button2.png';
+        }
+    });
+});
+
+function login() {
+    window.location.href = "login.html";
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
